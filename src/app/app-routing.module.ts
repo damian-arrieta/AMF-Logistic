9451bdda30f7e248d1e881365e463e853dfc637c
arrdamian <arrdamian@icloud.com>
2023-04-19 18:57:05 -0300

    Added search functionality

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'home', loadChildren: () => import('./components/pages/home/home.module').then(m => m.HomeModule) }, { path: 'character-detail', loadChildren: () => import('./components/pages/characters/character-detail/character-detail.module').then(m => m.CharacterDetailModule) }, { path: 'character-list', loadChildren: () => import('./components/pages/characters/character-list/character-list.module').then(m => m.CharacterListModule) }];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', loadChildren: () => import('./components/pages/home/home.module').then(m => m.HomeModule) },
+  { path: 'character-detail/:id', loadChildren: () => import('./components/pages/characters/character-detail/character-detail.module').then(m => m.CharacterDetailModule) },
+  { path: 'character-list', loadChildren: () => import('./components/pages/characters/character-list/character-list.module').then(m => m.CharacterListModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/shared/form/form.component.ts
+++ src/app/shared/form/form.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-form',
-  templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  templateUrl: './form.component.html'
 })
 export class FormComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onSearch(value: string) {
+    if(value && value.length > 3) {
+      this.router.navigate(['/character-list'], {
+        queryParams: { q:value }
+      });
+    }
   }
 
 }
