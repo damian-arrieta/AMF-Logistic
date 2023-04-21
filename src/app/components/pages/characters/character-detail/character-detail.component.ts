c6491085e441e24fd42629d38ee559ca58481deb
arrdamian <arrdamian@icloud.com>
2023-04-21 11:43:32 -0300

    Added funcionality and styles in character-detail component

--- src/app/components/pages/characters/character-detail/character-detail.component.ts
+++ src/app/components/pages/characters/character-detail/character-detail.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { Character } from '@app/shared/interfaces/character.interface';
+import { CharacterService } from '@app/shared/services/character.service';
+import { Observable, take } from 'rxjs';
 
 @Component({
   selector: 'app-character-detail',
@@ -6,10 +11,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./character-detail.component.css']
 })
 export class CharacterDetailComponent implements OnInit {
+  character$: Observable<Character> = new Observable<Character>();
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private characterSvc: CharacterService, private location: Location) { }
 
   ngOnInit(): void {
-  }
+    this.route.params.pipe(
+      take(1)
+    ).subscribe((params: any) => {
+      const id = params['id'];
+      this.character$ = this.characterSvc.getDetails(id);
+    });
+  }  
 
+  goBack(): void{
+    this.location.back();
+  }
 }
